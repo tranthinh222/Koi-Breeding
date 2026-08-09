c8c5519ec30db8d0ffd7ab4dcb056ed5e62b56e3
Trần Quốc Thịnh <tranquocthinh2221980@gmail.com>
2026-08-09 17:54:38 +0700

    first commit

---
+++ server/koi-breeding/src/main/java/com/koibreeding/KoiBreedingApplication.java
@@ -0,0 +1,13 @@
+package com.koibreeding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoiBreedingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KoiBreedingApplication.class, args);
+	}
+
+}
---
+++ server/koi-breeding/src/test/java/com/koibreeding/KoiBreedingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koibreeding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoiBreedingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

2e8b078ac7ce7295070563d43a973142405c58f8
hoanggiabao1804 <hoanggiabao1804@gmail.com>
2026-08-12 23:28:57 +0700

    (feat): Add Koi Row and Koi List to display all koi variants in admin page.

---
+++ client/src/components/KoiList/KoiList.tsx
@@ -0,0 +1,441 @@
+import { ArrowLeft, ArrowRight, ArrowUpDown, Search } from "lucide-react";
+import { useEffect, useRef, useState } from "react";
+import type { KoiVarient, PageDTO } from "../../types";
+import KoiForm from "../KoiForm/KoiForm";
+import KoiRow from "../KoiRow/KoiRow";
+import styles from "./KoiList.module.css";
+
+// export interface KoiVarient {
+// 	id: number;
+// 	name: string;
+// 	origin: string;
+// 	variety: string;
+// 	scaleType: string;
+// 	shape: string;
+// 	baseMaxLength: number;
+// 	baseGrowthRate: number;
+// 	midAge: number;
+// 	alphaWeight: number;
+// 	basePrice: number;
+// 	alphaPrice: number;
+// }
+
+// export interface PageDTO<T> {
+// 	page: number;
+// 	pageSize: number;
+// 	data: T[];
+// 	totalPages: number;
+// 	totalElements: number;
+// }
+
+const kois: KoiVarient[] = [
+	{
+		id: 1,
+		name: "Kohaku",
+		origin: "Japan",
+		variety: "Kohaku",
+		scaleType: "Wagoi",
+		shape: "Standard",
+		baseMaxLength: 90.0,
+		baseGrowthRate: 0.015,
+		midAge: 400,
+		alphaWeight: 0.000015,
+		basePrice: 100,
+		alphaPrice: 1.68,
+	},
+	{
+		id: 2,
+		name: "Kuchibeni Kohaku",
+		origin: "Japan",
+		variety: "Kohaku",
+		scaleType: "Wagoi",
+		shape: "Standard",
+		baseMaxLength: 90.0,
+		baseGrowthRate: 0.0149,
+		midAge: 400,
+		alphaWeight: 0.000015,
+		basePrice: 120,
+		alphaPrice: 1.7,
+	},
+	{
+		id: 3,
+		name: "Kuchibeni Kohaku",
+		origin: "Japan",
+		variety: "Kohaku",
+		scaleType: "Wagoi",
+		shape: "Standard",
+		baseMaxLength: 90.0,
+		baseGrowthRate: 0.0149,
+		midAge: 400,
+		alphaWeight: 0.000015,
+		basePrice: 120,
+		alphaPrice: 1.7,
+	},
+	{
+		id: 4,
+		name: "Kuchibeni Kohaku",
+		origin: "Japan",
+		variety: "Kohaku",
+		scaleType: "Wagoi",
+		shape: "Standard",
+		baseMaxLength: 90.0,
+		baseGrowthRate: 0.0149,
+		midAge: 400,
+		alphaWeight: 0.000015,
+		basePrice: 120,
+		alphaPrice: 1.7,
+	},
+	{
+		id: 5,
+		name: "Kuchibeni Kohaku",
+		origin: "Japan",
+		variety: "Kohaku",
+		scaleType: "Wagoi",
+		shape: "Standard",
+		baseMaxLength: 90.0,
+		baseGrowthRate: 0.0149,
+		midAge: 400,
+		alphaWeight: 0.000015,
+		basePrice: 120,
+		alphaPrice: 1.7,
+	},
+	{
+		id: 6,
+		name: "Kuchibeni Kohaku",
+		origin: "Japan",
+		variety: "Kohaku",
+		scaleType: "Wagoi",
+		shape: "Standard",
+		baseMaxLength: 90.0,
+		baseGrowthRate: 0.0149,
+		midAge: 400,
+		alphaWeight: 0.000015,
+		basePrice: 120,
+		alphaPrice: 1.7,
+	},
+	{
+		id: 7,
+		name: "Kuchibeni Kohaku",
+		origin: "Japan",
+		variety: "Kohaku",
+		scaleType: "Wagoi",
+		shape: "Standard",
+		baseMaxLength: 90.0,
+		baseGrowthRate: 0.0149,
+		midAge: 400,
+		alphaWeight: 0.000015,
+		basePrice: 120,
+		alphaPrice: 1.7,
+	},
+	{
+		id: 8,
+		name: "Kuchibeni Kohaku",
+		origin: "Japan",
+		variety: "Kohaku",
+		scaleType: "Wagoi",
+		shape: "Standard",
+		baseMaxLength: 90.0,
+		baseGrowthRate: 0.0149,
+		midAge: 400,
+		alphaWeight: 0.000015,
+		basePrice: 120,
+		alphaPrice: 1.7,
+	},
+	{
+		id: 9,
+		name: "Kuchibeni Kohaku",
+		origin: "Japan",
+		variety: "Kohaku",
+		scaleType: "Wagoi",
+		shape: "Standard",
+		baseMaxLength: 90.0,
+		baseGrowthRate: 0.0149,
+		midAge: 400,
+		alphaWeight: 0.000015,
+		basePrice: 120,
+		alphaPrice: 1.7,
+	},
+	{
+		id: 10,
+		name: "Kuchibeni Kohaku",
+		origin: "Japan",
+		variety: "Kohaku",
+		scaleType: "Wagoi",
+		shape: "Standard",
+		baseMaxLength: 90.0,
+		baseGrowthRate: 0.0149,
+		midAge: 400,
+		alphaWeight: 0.000015,
+		basePrice: 120,
+		alphaPrice: 1.7,
+	},
+	{
+		id: 11,
+		name: "Kuchibeni Kohaku",
+		origin: "Japan",
+		variety: "Kohaku",
+		scaleType: "Wagoi",
+		shape: "Standard",
+		baseMaxLength: 90.0,
+		baseGrowthRate: 0.0149,
+		midAge: 400,
+		alphaWeight: 0.000015,
+		basePrice: 120,
+		alphaPrice: 1.7,
+	},
+	{
+		id: 12,
+		name: "Kuchibeni Kohaku",
+		origin: "Japan",
+		variety: "Kohaku",
+		scaleType: "Wagoi",
+		shape: "Standard",
+		baseMaxLength: 90.0,
+		baseGrowthRate: 0.0149,
+		midAge: 400,
+		alphaWeight: 0.000015,
+		basePrice: 120,
+		alphaPrice: 1.7,
+	},
+	{
+		id: 13,
+		name: "Kuchibeni Kohaku",
+		origin: "Japan",
+		variety: "Kohaku",
+		scaleType: "Wagoi",
+		shape: "Standard",
+		baseMaxLength: 90.0,
+		baseGrowthRate: 0.0149,
+		midAge: 400,
+		alphaWeight: 0.000015,
+		basePrice: 120,
+		alphaPrice: 1.7,
+	},
+	{
+		id: 14,
+		name: "Kuchibeni Kohaku",
+		origin: "Japan",
+		variety: "Kohaku",
+		scaleType: "Wagoi",
+		shape: "Standard",
+		baseMaxLength: 90.0,
+		baseGrowthRate: 0.0149,
+		midAge: 400,
+		alphaWeight: 0.000015,
+		basePrice: 120,
+		alphaPrice: 1.7,
+	},
+	{
+		id: 15,
+		name: "Kuchibeni Kohaku",
+		origin: "Japan",
+		variety: "Kohaku",
+		scaleType: "Wagoi",
+		shape: "Standard",
+		baseMaxLength: 90.0,
+		baseGrowthRate: 0.0149,
+		midAge: 400,
+		alphaWeight: 0.000015,
+		basePrice: 120,
+		alphaPrice: 1.7,
+	},
+	{
+		id: 16,
+		name: "Kuchibeni Kohaku",
+		origin: "Japan",
+		variety: "Kohaku",
+		scaleType: "Wagoi",
+		shape: "Standard",
+		baseMaxLength: 90.0,
+		baseGrowthRate: 0.0149,
+		midAge: 400,
+		alphaWeight: 0.000015,
+		basePrice: 120,
+		alphaPrice: 1.7,
+	},
+];
+
+function KoiList() {
+	const [page, setPage] = useState<number>(1);
+	const [pageSize, setPageSize] = useState<number>(10);
+	const [totalPages, setTotalPages] = useState<number>(1);
+	const [data, setData] = useState<KoiVarient[]>([]);
+	const [isCreateDialogOpen, setIsCreateDialogOpen] =
+		useState<boolean>(false);
+
+	useEffect(() => {
+		const loadData = async () => {
+			try {
+				const pageDTO = await fetchData(page, pageSize);
+				setData(pageDTO.data);
+				setTotalPages(pageDTO.totalPages);
+			} catch (error) {
+				console.error("Failed to fetch data:", error);
+			}
+		};
+
+		listRef.current?.scrollTo({
+			top: 0,
+			behavior: "smooth",
+		});
+
+		loadData();
+	}, [page, pageSize, data.length]);
+
+	const listRef = useRef<HTMLDivElement>(null);
+
+	const fetchData = async (
+		page: number,
+		pageSize: number,
+	): Promise<PageDTO<KoiVarient>> => {
+		const startIndex = (page - 1) * pageSize;
+		const endIndex = startIndex + pageSize;
+		const pageData = kois.slice(startIndex, endIndex);
+
+		return {
+			page: page,
+			pageSize: pageSize,
+			data: pageData,
+			totalPages: Math.ceil(kois.length / pageSize),
+			totalElements: kois.length,
+		};
+	};
+
+	const handlePageChange = (newPage: number) => {
+		if (Number.isNaN(newPage)) {
+			return;
+		}
+
+		setPage(Math.max(1, newPage));
+	};
+
+	return (
+		<>
+			<div className={styles.container}>
+				<div className={`${styles.cloud} ${styles.cloud1}`}></div>
+				<div className={`${styles.cloud} ${styles.cloud2}`}></div>
+				<div className={`${styles.cloud} ${styles.cloud3}`}></div>
+
+				<div className={styles.background}>
+					<div className={styles.grass}></div>
+
+					<div className={`${styles.pond} ${styles.pond1}`}></div>
+					<div className={`${styles.pond} ${styles.pond2}`}></div>
+					<div className={`${styles.pond} ${styles.pond3}`}></div>
+
+					<div
+						className={`${styles.trees} ${styles.treesLeft}`}
+					></div>
+
+					<div
+						className={`${styles.trees} ${styles.treesRight}`}
+					></div>
+				</div>
+				<div className={styles.header}>
+					<div className={styles.searchPanel}>
+						<div className={styles.searchWrapper}>
+							<Search size="30" color="#a9acb1" />
+							<input
+								type="text"
+								placeholder="Search by name, origin, variety, ..."
+							/>
+						</div>
+						<button type="button" className={styles.searchButton}>
+							<Search size="30" color="#ffffff" />
+						</button>
+						<button type="button" className={styles.searchButton}>
+							<ArrowUpDown size="30" color="#ffffff" />
+						</button>
+					</div>
+					<button
+						type="button"
+						className={styles.createButton}
+						onClick={() => setIsCreateDialogOpen(true)}
+					>
+						Create Koi
+					</button>
+				</div>
+				<div ref={listRef} className={styles.list}>
+					{data.map((koi, index) => (
+						<KoiRow key={index} koi={koi} />
+					))}
+				</div>
+				<div className={styles.footer}>
+					<div className={styles.pageSizeSelect}>
+						<span>Items per page</span>
+						<select
+							value={pageSize}
+							onChange={(e) => {
+								const newPageSize = Number.parseInt(
+									e.target.value,
+								);
+
+								setPageSize(newPageSize);
+								setPage(1);
+							}}
+						>
+							<option value="10">10 items</option>
+							<option value="20">20 items</option>
+							<option value="50">50 items</option>
+							<option value="100">100 items</option>
+						</select>
+					</div>
+					<div className={styles.pageSelect}>
+						<div className={styles.pageInput}>
+							<span>Page</span>
+							<input
+								value={page}
+								type="number"
+								min="1"
+								max={totalPages}
+								onChange={(e) =>
+									handlePageChange(
+										Number.parseInt(e.target.value),
+									)
+								}
+							/>
+						</div>
+						<div className={styles.pageButtons}>
+							<button
+								type="button"
+								className={styles.prevButton}
+								disabled={page == 1}
+								onClick={() =>
+									handlePageChange(Math.max(1, page - 1))
+								}
+							>
+								<ArrowLeft />
+							</button>
+							<button
+								type="button"
+								className={styles.nextButton}
+								disabled={page == totalPages}
+								onClick={() => {
+									handlePageChange(
+										Math.min(totalPages, page + 1),
+									);
+								}}
+							>
+								<ArrowRight />
+							</button>
+						</div>
+					</div>
+				</div>
+			</div>
+			{isCreateDialogOpen ? (
+				<div className={styles.overlay}>
+					<KoiForm
+						koi={null}
+						onClose={() => setIsCreateDialogOpen(false)}
+						onSubmit={(koi) => {
+							kois.unshift({ ...koi, id: kois.length + 1 });
+							data.unshift({ ...koi, id: kois.length + 1 });
+						}}
+					/>
+				</div>
+			) : null}
+		</>
+	);
+}
+
+export default KoiList;
---
+++ client/src/components/KoiRow/KoiRow.tsx
@@ -0,0 +1,154 @@
+import {
+	BanknoteArrowUp,
+	ChevronsUp,
+	ClockArrowUp,
+	Coins,
+	Earth,
+	RulerDimensionLine,
+	SquarePen,
+	Weight,
+} from "lucide-react";
+import { useState } from "react";
+import type { KoiVarient } from "../../types";
+import KoiForm from "../KoiForm/KoiForm";
+import styles from "./KoiRow.module.css";
+
+interface KoiDictionaryCardProps {
+	koi: KoiVarient;
+}
+
+function KoiRow({ koi }: KoiDictionaryCardProps) {
+	const [isUpdateDialogOpen, setIsUpdateDialogOpen] =
+		useState<boolean>(false);
+
+	return (
+		<>
+			<div className={styles.card}>
+				<section className={styles.image}>
+					<img
+						src={`/kois/${koi.name.toLowerCase().replace(" ", "-")}.png`}
+						alt="koi"
+					/>
+				</section>
+				<div className={styles.content}>
+					<div className={styles.titleBar}>
+						<span className={styles.name}>{koi.name}</span>
+						<button
+							type="button"
+							className={styles.editButton}
+							onClick={() => setIsUpdateDialogOpen(true)}
+						>
+							<SquarePen size="30" />
+						</button>
+					</div>
+					<div className={styles.attributes}>
+						<section className={styles.leftSection}>
+							<div className={styles.fieldWrapper}>
+								<span className={styles.fieldLabel}>
+									ID: {koi.id}
+								</span>
+							</div>
+							<div className={styles.fieldWrapper}>
+								<Earth size="30" color="#096649" />
+								<span>
+									<strong>Origin:</strong> {koi.origin}
+								</span>
+							</div>
+							<div className={styles.fieldWrapper}>
+								<span className={styles.fieldLabel}>
+									Variety:
+								</span>
+								<span className={styles.varietyBadge}>
+									{koi.variety}
+								</span>
+							</div>
+							<div className={styles.fieldWrapper}>
+								<span className={styles.fieldLabel}>
+									Scale Type:
+								</span>
+								<span className={styles.scaleBadge}>
+									{koi.scaleType}
+								</span>
+							</div>
+							<div className={styles.fieldWrapper}>
+								<span className={styles.fieldLabel}>
+									Shape:
+								</span>
+								<span className={styles.shapeBadge}>
+									{koi.shape}
+								</span>
+							</div>
+						</section>
+						<section className={styles.rightSection}>
+							<div className={styles.fieldWrapper}>
+								<RulerDimensionLine size="30" color="#b49429" />
+								<span>
+									<strong>Base Max Length:</strong>{" "}
+									{koi.baseMaxLength} cm
+								</span>
+							</div>
+							<div className={styles.fieldWrapper}>
+								<ChevronsUp size="30" color="#44d131" />
+								<span>
+									<strong>Base Growth Rate:</strong>{" "}
+									{koi.baseGrowthRate}
+								</span>
+							</div>
+							<div className={styles.fieldWrapper}>
+								<ClockArrowUp size="30" color="#aeb11a" />
+								<span>
+									<strong>Mid Age:</strong> {koi.midAge} days
+								</span>
+							</div>
+							<div className={styles.fieldWrapper}>
+								<Weight size="30" />
+								<span>
+									<strong>Alpha Weight:</strong>{" "}
+									{koi.alphaWeight}
+								</span>
+							</div>
+							<div className={styles.fieldWrapper}>
+								<Coins size="30" color="#d0d236" />
+								<span>
+									<strong>Base Price:</strong>{" "}
+									{koi.basePrice}{" "}
+								</span>
+								<Coins size="24" color="#d0d236" />
+							</div>
+							<div className={styles.fieldWrapper}>
+								<BanknoteArrowUp size="30" />
+								<span>
+									<strong>Alpha Price:</strong>{" "}
+									{koi.alphaPrice}
+								</span>
+							</div>
+						</section>
+					</div>
+				</div>
+			</div>
+			{isUpdateDialogOpen ? (
+				<div className={styles.overlay}>
+					<KoiForm
+						koi={koi}
+						onClose={() => setIsUpdateDialogOpen(false)}
+						onSubmit={(requestKoi) => {
+							koi.name = requestKoi.name;
+							koi.origin = requestKoi.origin;
+							koi.variety = requestKoi.variety;
+							koi.scaleType = requestKoi.scaleType;
+							koi.shape = requestKoi.shape;
+							koi.baseMaxLength = requestKoi.baseMaxLength;
+							koi.baseGrowthRate = requestKoi.baseGrowthRate;
+							koi.midAge = requestKoi.midAge;
+							koi.alphaWeight = requestKoi.alphaWeight;
+							koi.basePrice = requestKoi.basePrice;
+							koi.alphaPrice = requestKoi.alphaPrice;
+						}}
+					/>
+				</div>
+			) : null}
+		</>
+	);
+}
+
+export default KoiRow;

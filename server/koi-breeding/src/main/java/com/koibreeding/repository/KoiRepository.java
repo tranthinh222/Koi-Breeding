c110cd299dca07d29fa6c0485eaa5d4221238460
Nguyễn Văn Phước <nguyenthaibinh0123@outlook.com.vn>
2026-08-13 12:09:57 +0700

    (response) Add KoiSummary class for koi data representation

--- server/koi-breeding/src/main/java/com/koibreeding/repository/KoiRepository.java
+++ server/koi-breeding/src/main/java/com/koibreeding/repository/KoiRepository.java
@@ -1,8 +1,11 @@
 package com.koibreeding.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.koibreeding.domain.Koi;
 
 public interface KoiRepository extends JpaRepository<Koi, Integer> {
+    List<Koi> findTop3ByOrderByIdDesc();
 }

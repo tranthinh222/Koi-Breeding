6c7b65427ffc0300687a586f84811d27304be5d1
Nguyễn Văn Phước <nguyenthaibinh0123@outlook.com.vn>
2026-08-13 16:50:10 +0700

    (controller) Enhance HomeController to fetch user-specific home data

--- server/koi-breeding/src/main/java/com/koibreeding/controller/HomeController.java
+++ server/koi-breeding/src/main/java/com/koibreeding/controller/HomeController.java
@@ -2,6 +2,7 @@ package com.koibreeding.controller;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.koibreeding.domain.response.home.HomeResponse;
 import com.koibreeding.service.HomeService;
@@ -16,8 +17,8 @@ public class HomeController {
     }
 
     @GetMapping("/home")
-    public ResponseEntity<HomeResponse> getHome(){
-        HomeResponse response = homeService.getHome();
+    public ResponseEntity<HomeResponse> getHome(@RequestParam Integer id){
+        HomeResponse response = homeService.getHome(id);
 
         return ResponseEntity.ok(response);
     }

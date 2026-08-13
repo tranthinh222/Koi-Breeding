4f21d5866a9da5dc8ceb8e449b29c2c7fa158f16
hoanggiabao1804 <hoanggiabao1804@gmail.com>
2026-08-13 15:46:19 +0700

    (domain): Add AllArgsContructor annotation for creating new object with arguments constructor.

--- server/koi-breeding/src/main/java/com/koibreeding/domain/Variety.java
+++ server/koi-breeding/src/main/java/com/koibreeding/domain/Variety.java
@@ -6,6 +6,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -15,6 +16,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class Variety {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)

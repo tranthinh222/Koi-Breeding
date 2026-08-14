7b06a92b518f11b2413499c2407116197c6988ec
hoanggiabao1804 <hoanggiabao1804@gmail.com>
2026-08-14 17:09:01 +0700

    (api): Add api methods for variety module.

--- client/src/config/api.ts
+++ client/src/config/api.ts
@@ -1,11 +1,10 @@
+import type { IKoiVarient, IModelPagination, IVariety } from "../types/backend";
 import axios from "./axios-customize";
 
 /**
  * 
 Module Koi Dictionary
  */
-
-import type { IKoiVarient, IModelPagination } from "../types/backend";
 
 export const callCreateKoiVarient = (koiVarient: IKoiVarient) => {
 	return axios.post<IKoiVarient>("/api/v1/dictionaries", { ...koiVarient });
@@ -23,4 +22,25 @@ export const callFetchKoiVarient = (query: string) => {
 
 export const callFetchKoiVarientById = (id: number) => {
 	return axios.get<IKoiVarient>(`/api/v1/dictionaries/${id}`);
+};
+
+/**
+ * 
+Module Variety
+ */
+
+export const callCreateVariety = (variety: IVariety) => {
+	return axios.post<IVariety>("/api/v1/varieties", { ...variety });
+};
+
+export const callUpdateVariety = (variety: IVariety) => {
+	return axios.put<IVariety>("/api/v1/varieties", { ...variety });
+};
+
+export const callFetchAllVarieties = (query: string) => {
+	return axios.get<IModelPagination<IVariety>>(`/api/v1/varieties?${query}`);
+};
+
+export const callFetchVarietyById = (id: number) => {
+	return axios.get<IVariety>(`/api/v1/varieties/${id}`);
 };

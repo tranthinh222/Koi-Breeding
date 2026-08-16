9f742d0a41afc4f96860ca8de9f659ff603a7f9c
hoanggiabao1804 <hoanggiabao1804@gmail.com>
2026-08-16 08:28:29 +0700

    (test): Add intergration test for KoiDictionaryController.

--- server/koi-breeding/src/test/java/com/koibreeding/KoiBreedingApplicationTests.java
+++ server/koi-breeding/src/test/java/com/koibreeding/KoiBreedingApplicationTests.java
@@ -1,13 +1,27 @@
 package com.koibreeding;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class KoiBreedingApplicationTests {
+
+	@Value("${spring.application.name}")
+	private String title;
 
 	@Test
 	void contextLoads() {
+		System.out.println(">>> Run in " + title);
+	}
+
+	@Test
+	void checkApplicationProperties() {
+		assertEquals("koi-breeding-test", title);
 	}
 
 }

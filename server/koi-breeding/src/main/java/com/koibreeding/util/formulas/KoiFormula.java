2740a2d5e5bf7d5c53aff1362f569be3e517f5c6
hoanggiabao1804 <hoanggiabao1804@gmail.com>
2026-08-27 23:39:56 +0700

    (feat): Add utilities for Koi and Pond formulas.

---
+++ server/koi-breeding/src/main/java/com/koibreeding/util/formulas/KoiFormula.java
@@ -0,0 +1,215 @@
+package com.koibreeding.util.formulas;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Random;
+
+import com.koibreeding.domain.Dictionary;
+import com.koibreeding.domain.Koi;
+import com.koibreeding.domain.Mutation;
+import com.koibreeding.domain.Pond;
+import com.koibreeding.enums.Gender;
+import com.koibreeding.enums.LifeStage;
+import com.koibreeding.enums.Shape;
+
+public class KoiFormula {
+    private static final Random random = new Random();
+
+    private static double generateGaussianInRange(double min, double max) {
+        double mean = (min + max) / 2.0;
+        double stdDev = (max - min) / 6.0;
+
+        double val;
+        do {
+            val = mean + (random.nextGaussian() * stdDev);
+        } while (val < min || val > max); // Remove cases that exceed the allowed range
+
+        return val;
+    }
+
+    public static LifeStage defineLifeStage(int age) {
+        if (0 <= age && age <= 3) {
+            return LifeStage.EGG;
+        }
+
+        if (age > 3 && age <= 10) {
+            return LifeStage.LARVA;
+        }
+
+        if (age > 10 && age <= 30) {
+            return LifeStage.FRY;
+        }
+
+        if (age > 30 && age < 6 * 30) {
+            return LifeStage.JUVENILE;
+        }
+
+        return LifeStage.ADULT;
+    }
+
+    public static BigDecimal generateRandomPotential() {
+        return BigDecimal.valueOf(generateGaussianInRange(0.85, 1.3));
+    }
+
+    public static BigDecimal calculateKoiLength(int age, int health, int foodBar, int cureBar, BigDecimal potential,
+            Mutation mutation,
+            Dictionary dictionary, Pond pond) {
+        // Egg size vary from 1.2 - 2.0 mm
+        if (defineLifeStage(age).equals(LifeStage.EGG)) {
+            return BigDecimal.valueOf(generateGaussianInRange(0.12, 0.2)).setScale(2, RoundingMode.HALF_UP);
+        }
+
+        // Larva_Length = 10 * BaseGrowthRate * (age + 1)
+        if (defineLifeStage(age).equals(LifeStage.LARVA)) {
+            return BigDecimal.valueOf(calculateLarvaLength(age, dictionary)).setScale(2,
+                    RoundingMode.HALF_UP);
+        }
+
+        // Fry_length = Larva_length(10) * e ^ (4 * BaseGrowthRate * (age - 10))
+        if (defineLifeStage(age).equals(LifeStage.FRY)) {
+            return BigDecimal
+                    .valueOf(calculateFryLength(age, dictionary))
+                    .setScale(2, RoundingMode.HALF_UP);
+        }
+
+        /**
+         * Juvenile & Adult
+         * Max_Length_effective = BaseMaxLength * PotentialModifier * MutationModifier
+         * k_effective = BaseGrowthRate * EnvironmentModifier * NutritionModifier *
+         * CureModifier * HealthModifier
+         * Length(age) = (Max_Length_effective) / (1 + e ^ (-k_effective * (age -
+         * Mid_Age)))
+         * Normalized_Length(age) = (Length(age) - Length(30)) / (1 - Length(30))
+         * CurrentLength(age) = Fry_Length(30) + (Max_Length_effective - Fry_Length(30))
+         * * Normalized_Length(age)
+         */
+
+        double baseMaxLength = dictionary.getBaseMaxLength().doubleValue();
+        double potentialModifier = potential.doubleValue();
+        double mutationModifier = mutation != null ? mutation.getValue().doubleValue() : 1.0;
+        double baseGrowthRate = dictionary.getBaseGrowthRate().doubleValue();
+        double environmentModifier = pond != null ? 0.8
+                + (PondFormula.getWaterQualityScore(pond.getPH(), pond.getTemperature(), pond.getOxygen()) / 100.0)
+                        * 0.4
+                : 1.0;
+        double nutritionModifier = 0.85 + (foodBar / 100.0) * 0.3;
+        double cureModifier = 0.7 + (cureBar / 100.0) * 0.3;
+        double healthModifier = 0.5 + (health / 100.0) * 0.5;
+        int midAge = dictionary.getMidAge();
+
+        double maxLengthEffective = baseMaxLength * potentialModifier * mutationModifier;
+        double kEffective = baseGrowthRate * environmentModifier * nutritionModifier * cureModifier * healthModifier;
+        double lengthAge = calculateLength(maxLengthEffective, kEffective, age, midAge);
+        double fryLength30 = calculateFryLength(30, dictionary);
+        double length30 = calculateLength(maxLengthEffective, kEffective, 30, midAge);
+        double normalizedLengthAge = (lengthAge - length30) / (1 - length30);
+        double currentLengthAge = fryLength30 + (maxLengthEffective - fryLength30) * normalizedLengthAge;
+
+        return BigDecimal.valueOf(currentLengthAge).setScale(2, RoundingMode.HALF_UP);
+    }
+
+    private static double calculateLarvaLength(int age, Dictionary dictionary) {
+        return 10 * dictionary.getBaseGrowthRate().doubleValue() * (age + 1);
+    }
+
+    private static double calculateFryLength(int age, Dictionary dictionary) {
+        double larvaLength10 = calculateLarvaLength(10, dictionary);
+        return larvaLength10 * Math.exp(4 * dictionary.getBaseGrowthRate().doubleValue() * (age - 10));
+    }
+
+    private static double calculateLength(double maxLengthEffective, double kEffective, int age, int midAge) {
+        return maxLengthEffective / (1 + Math.exp(-kEffective * (age - midAge)));
+    }
+
+    public static BigDecimal calculateKoiWeight(double length, BigDecimal potential, int foodBar, int health, Pond pond,
+            Dictionary dictionary) {
+        double alpha = dictionary.getAlphaWeight().doubleValue();
+        double beta = generateGaussianInRange(2.95, 3.0);
+        double bodyTypeModifier = dictionary.getShape().equals(Shape.STANDARD) ? 1.0 : 0.94;
+        double potentialModifier = potential.doubleValue();
+        double environmentModifier = (pond != null) ? 0.8
+                + (PondFormula.getWaterQualityScore(pond.getPH(), pond.getTemperature(), pond.getOxygen()) / 100.0)
+                        * 0.4
+                : 1.0;
+        double nutritionModifier = 0.85 + (foodBar / 100.0) * 0.3;
+        double healthModifier = 0.5 + (health / 100.0) * 0.5;
+
+        double bodyCondition = potentialModifier * environmentModifier * nutritionModifier * healthModifier;
+
+        return BigDecimal.valueOf(alpha * Math.pow(length, beta) * bodyTypeModifier * bodyCondition).setScale(2,
+                RoundingMode.HALF_UP);
+    }
+
+    public static int calculateKoiPrice(int age, double length, int patternScore, int colorScore, int bodyScore,
+            int skinScore, int scaleScore, int health, Mutation mutation, Dictionary dictionary) {
+        int midAge = dictionary.getMidAge();
+        int basePrice = dictionary.getBasePrice();
+        double alpha = dictionary.getAlphaPrice().doubleValue();
+        double lengthBase = 10.0;
+        double beautyScore = 0.35 * patternScore + 0.25 * colorScore + 0.2 * bodyScore + 0.1 * skinScore
+                + 0.1 * scaleScore;
+        double beautyModifier = 1.2 * (beautyScore / 100.0) + 0.8;
+        double qualityModifier = 0.4 * (health / 100.0) + 0.8;
+        double ageModifier = calculateAgeModifier(age, midAge);
+        double mutationModifier = mutation != null ? mutation.getValue().doubleValue() : 1.0;
+        double varience = generateGaussianInRange(0.95, 1.05);
+
+        double price = basePrice * Math.pow((length / lengthBase), alpha) * beautyModifier * qualityModifier
+                * ageModifier * mutationModifier * varience;
+        return BigDecimal.valueOf(price).setScale(0, RoundingMode.HALF_UP).intValue();
+
+    }
+
+    private static double calculateAgeModifier(int age, int midAge) {
+        double ageModifier = 0.9 + (1.1 - 0.9) * Math.exp(-Math.pow((age - midAge) / 365.0, 2) / (2 * Math.pow(2, 2)));
+
+        if (ageModifier < 0.9) {
+            return 0.9;
+        }
+
+        if (ageModifier > 1.1) {
+            return 1.1;
+        }
+
+        return ageModifier;
+    }
+
+    public static Koi generateStarterKoi(Dictionary dictionary) {
+        Koi koi = new Koi();
+
+        int age = 50;
+        int health = (int) generateGaussianInRange(80, 100);
+        int foodBar = (int) generateGaussianInRange(60, 80);
+        int cureBar = 100;
+        BigDecimal potential = generateRandomPotential();
+        int patternScore = (int) generateGaussianInRange(60.0, 100.0);
+        int colorScore = (int) generateGaussianInRange(60.0, 100.0);
+        int bodyScore = (int) generateGaussianInRange(60.0, 100.0);
+        int skinScore = (int) generateGaussianInRange(60.0, 100.0);
+        int scaleScore = (int) generateGaussianInRange(60.0, 100.0);
+        BigDecimal length = calculateKoiLength(age, health, foodBar, cureBar, potential, null, dictionary, null);
+        BigDecimal weight = calculateKoiWeight(length.doubleValue(), potential, foodBar, health, null, dictionary);
+        int price = calculateKoiPrice(age, length.doubleValue(), patternScore, colorScore, bodyScore, skinScore,
+                scaleScore, health, null, dictionary);
+
+        koi.setName(dictionary.getName());
+        koi.setAge(age);
+        koi.setLength(length);
+        koi.setWeight(weight);
+        koi.setHealth(health);
+        koi.setFoodBar(foodBar);
+        koi.setCureBar(cureBar);
+        koi.setGender(generateGaussianInRange(-1.0, 1.0) < 0 ? Gender.MALE : Gender.FEMALE);
+        koi.setPrice(price);
+        koi.setLifeStage(defineLifeStage(age));
+        koi.setPotential(potential);
+        koi.setDictionary(dictionary);
+        koi.setPatternScore(patternScore);
+        koi.setColorScore(colorScore);
+        koi.setBodyScore(bodyScore);
+        koi.setSkinScore(skinScore);
+        koi.setScaleScore(scaleScore);
+
+        return koi;
+    }
+}
---
+++ server/koi-breeding/src/main/java/com/koibreeding/util/formulas/PondFormula.java
@@ -0,0 +1,143 @@
+package com.koibreeding.util.formulas;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class PondFormula {
+    private static final BigDecimal wPH = BigDecimal.valueOf(0.3);
+    private static final BigDecimal wTemperature = BigDecimal.valueOf(0.3);
+    private static final BigDecimal wOxygen = BigDecimal.valueOf(0.4);
+
+    private static final BigDecimal[] pHList = {
+            BigDecimal.valueOf(6.0),
+            BigDecimal.valueOf(6.5),
+            BigDecimal.valueOf(6.8),
+            BigDecimal.valueOf(7.0),
+            BigDecimal.valueOf(7.5),
+            BigDecimal.valueOf(8.0),
+            BigDecimal.valueOf(8.5),
+            BigDecimal.valueOf(9.0),
+    };
+
+    private static final BigDecimal[] temperatureList = {
+            BigDecimal.valueOf(10.0),
+            BigDecimal.valueOf(15.0),
+            BigDecimal.valueOf(20.0),
+            BigDecimal.valueOf(22.0),
+            BigDecimal.valueOf(26.0),
+            BigDecimal.valueOf(28.0),
+            BigDecimal.valueOf(30.0),
+            BigDecimal.valueOf(32.0),
+    };
+
+    private static final BigDecimal[] dissolvedOxygenList = {
+            BigDecimal.valueOf(3.0),
+            BigDecimal.valueOf(4.0),
+            BigDecimal.valueOf(5.0),
+            BigDecimal.valueOf(6.0),
+            BigDecimal.valueOf(9.0),
+            BigDecimal.valueOf(10.0),
+            BigDecimal.valueOf(12.0),
+    };
+
+    public static int getPHScore(BigDecimal pH) {
+        if (pH.compareTo(pHList[0]) < 0 || pH.compareTo(pHList[7]) > 0) {
+            return 0;
+        }
+
+        if ((pH.compareTo(pHList[0]) >= 0 && pH.compareTo(pHList[1]) < 0)
+                || (pH.compareTo(pHList[6]) > 0 && pH.compareTo(pHList[7]) <= 0)) {
+            return 20;
+        }
+
+        if (pH.compareTo(pHList[1]) >= 0 && pH.compareTo(pHList[2]) < 0) {
+            return 50;
+        }
+
+        if (pH.compareTo(pHList[5]) > 0 && pH.compareTo(pHList[6]) <= 0) {
+            return 60;
+        }
+
+        if (pH.compareTo(pHList[2]) >= 0 && pH.compareTo(pHList[3]) < 0) {
+            return 80;
+        }
+
+        if (pH.compareTo(pHList[4]) > 0 && pH.compareTo(pHList[5]) <= 0) {
+            return 90;
+        }
+
+        return 100;
+    }
+
+    public static int getTemperatureScore(BigDecimal temperature) {
+        if (temperature.compareTo(temperatureList[0]) < 0 || temperature.compareTo(temperatureList[7]) > 0) {
+            return 0;
+        }
+
+        if (temperature.compareTo(temperatureList[0]) >= 0 && temperature.compareTo(temperatureList[1]) < 0) {
+            return 20;
+        }
+
+        if (temperature.compareTo(temperatureList[6]) > 0 && temperature.compareTo(temperatureList[7]) <= 0) {
+            return 40;
+        }
+
+        if (temperature.compareTo(temperatureList[1]) >= 0 && temperature.compareTo(temperatureList[2]) < 0) {
+            return 50;
+        }
+
+        if (temperature.compareTo(temperatureList[5]) > 0 && temperature.compareTo(temperatureList[6]) <= 0) {
+            return 70;
+        }
+
+        if (temperature.compareTo(temperatureList[2]) >= 0 && temperature.compareTo(temperatureList[3]) < 0) {
+            return 80;
+        }
+
+        if (temperature.compareTo(temperatureList[4]) > 0 && temperature.compareTo(temperatureList[5]) <= 0) {
+            return 90;
+        }
+
+        return 100;
+    }
+
+    public static int getOxygenScore(BigDecimal oxygen) {
+        if (oxygen.compareTo(dissolvedOxygenList[0]) < 0) {
+            return 0;
+        }
+
+        if (oxygen.compareTo(dissolvedOxygenList[0]) >= 0 && oxygen.compareTo(dissolvedOxygenList[1]) < 0) {
+            return 20;
+        }
+
+        if ((oxygen.compareTo(dissolvedOxygenList[1]) >= 0 && oxygen.compareTo(dissolvedOxygenList[2]) < 0)
+                || oxygen.compareTo(dissolvedOxygenList[6]) > 0) {
+            return 50;
+        }
+
+        if (oxygen.compareTo(dissolvedOxygenList[5]) > 0 && oxygen.compareTo(dissolvedOxygenList[6]) <= 0) {
+            return 70;
+        }
+
+        if (oxygen.compareTo(dissolvedOxygenList[2]) >= 0 && oxygen.compareTo(dissolvedOxygenList[3]) < 0) {
+            return 80;
+        }
+
+        if (oxygen.compareTo(dissolvedOxygenList[4]) > 0 && oxygen.compareTo(temperatureList[5]) <= 0) {
+            return 90;
+        }
+
+        return 100;
+    }
+
+    public static int getWaterQualityScore(BigDecimal pH, BigDecimal temperature, BigDecimal oxygen) {
+        BigDecimal phScore = BigDecimal.valueOf(getPHScore(pH));
+        BigDecimal temperatureScore = BigDecimal.valueOf(getTemperatureScore(temperature));
+        BigDecimal oxygenScore = BigDecimal.valueOf(getOxygenScore(oxygen));
+        BigDecimal op1 = wPH.multiply(phScore);
+        BigDecimal op2 = wTemperature.multiply(temperatureScore);
+        BigDecimal op3 = wOxygen.multiply(oxygenScore);
+
+        return op1.add(op2).add(op3).setScale(0, RoundingMode.HALF_UP).intValue();
+    }
+}

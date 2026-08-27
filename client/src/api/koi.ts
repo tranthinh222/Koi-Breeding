f5c34584bc4a9428f463ca535d4687b74957674d
hoanggiabao1804 <hoanggiabao1804@gmail.com>
2026-08-27 23:36:49 +0700

    (api): Add new koi api handlers to release koi to pond and fetch kois in pond.

---
+++ client/src/api/koi.ts
@@ -0,0 +1,22 @@
+import type { IKoi, IRestResponse } from "../types/backend";
+import { apiClient } from "./client";
+
+export interface IRequestReleaseKoiDTO {
+	pondId: number;
+	inventoryId: number;
+	quantity: number;
+}
+
+export const callReleaseKoiToPond = (
+	requestReleaseKoiDTO: IRequestReleaseKoiDTO,
+) => {
+	return apiClient.post<IRestResponse<IKoi[]>>("/api/v1/kois/import", {
+		...requestReleaseKoiDTO,
+	});
+};
+
+export const callFetchKoisInPond = (pondId: number) => {
+	return apiClient.get<IRestResponse<IKoi[]>>(
+		`/api/v1/kois&pondId=${pondId}`,
+	);
+};

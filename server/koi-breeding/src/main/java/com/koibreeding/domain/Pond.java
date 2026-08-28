887b2202f1307d0941aab9868d29ebafd33733aa
hoanggiabao1804 <hoanggiabao1804@gmail.com>
2026-08-28 21:52:58 +0700

    (domain): Add all arguments constructor for Item and Pond.

--- server/koi-breeding/src/main/java/com/koibreeding/domain/Pond.java
+++ server/koi-breeding/src/main/java/com/koibreeding/domain/Pond.java
@@ -13,6 +13,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -22,6 +23,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
+@AllArgsConstructor
 public class Pond {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)

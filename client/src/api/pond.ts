4d62e2ec423791ee025cca2c6b6aeb9306ce16c4
hoanggiabao1804 <hoanggiabao1804@gmail.com>
2026-09-01 07:59:35 +0700

    (api): Add upgrade pond API call.

--- client/src/api/pond.ts
+++ client/src/api/pond.ts
@@ -3,28 +3,44 @@
 Module Pond
  */
 
-import type { IModelPagination, IPond, IRestResponse } from '../types/backend'
-import { apiClient } from './client'
+import type { IModelPagination, IPond, IRestResponse } from "../types/backend";
+import { apiClient } from "./client";
 
 export interface IRequestBuyPondDTO {
-  name: string
-  description: string
-  price: number
-  ownerId: number
+	name: string;
+	description: string;
+	price: number;
+	ownerId: number;
+}
+
+export interface IResponseBuyPondDTO {
+	pond: IPond;
+	balance: number;
+}
+
+export interface IResponseUpgradePondDTO {
+	pond: IPond;
+	balance: number;
 }
 
 export const callBuyPond = (requestBuyPondDTO: IRequestBuyPondDTO) => {
-  return apiClient.post<IRestResponse<IPond>>('/ponds', {
-    ...requestBuyPondDTO,
-  })
-}
+	return apiClient.post<IRestResponse<IResponseBuyPondDTO>>("/ponds", {
+		...requestBuyPondDTO,
+	});
+};
 
 export const callFetchAllPonds = (query: string) => {
-  return apiClient.get<IRestResponse<IModelPagination<IPond>>>(
-    `/ponds?${query}`,
-  )
-}
+	return apiClient.get<IRestResponse<IModelPagination<IPond>>>(
+		`/ponds?${query}`,
+	);
+};
 
 export const callUpdatePond = (pond: IPond) => {
-  return apiClient.put<IRestResponse<IPond>>('/ponds', { ...pond })
-}
+	return apiClient.put<IRestResponse<IPond>>("/ponds", { ...pond });
+};
+
+export const callUpgradePond = (pondId: number) => {
+	return apiClient.put<IRestResponse<IResponseUpgradePondDTO>>(
+		`/ponds/upgrade?pondId=${pondId}`,
+	);
+};

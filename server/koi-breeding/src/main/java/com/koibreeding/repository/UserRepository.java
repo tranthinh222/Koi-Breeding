9dae5a9aeebc081ca43289636a93380822f3c15a
Nguyễn Văn Phước <nguyenthaibinh0123@outlook.com.vn>
2026-09-04 19:17:01 +0700

    (repository): add queries to find and count users for admin dashboard

--- server/koi-breeding/src/main/java/com/koibreeding/repository/UserRepository.java
+++ server/koi-breeding/src/main/java/com/koibreeding/repository/UserRepository.java
@@ -1,14 +1,25 @@
 package com.koibreeding.repository;
 
 import com.koibreeding.domain.User;
+import com.koibreeding.enums.Role;
+
+import java.time.Instant;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+    Page<User> findAllByRole(Role role, Pageable pageable);
     boolean existsByUsername(String username);
     boolean existsByEmail(String email);
     Optional<User> findByUsername(String username);
     Optional<User> findByEmail(String email);
+    long countByCreatedAtBetween(Instant start, Instant end);
+    List<User> findTopByOrderByExpDesc();
+    List<User> findTop3ByOrderByExpDesc();
+    Optional<User> findFirstByOrderByExpDesc();
 
 }
